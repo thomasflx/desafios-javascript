cae9fcf73f251b469bd2960487be0b2ff3623ebf
Thomas Ewerton <ewerton.thomas@gmail.com>
2021-01-29 17:42:15 -0300

    No processo

--- challenges/6/challenge.js
+++ challenges/6/challenge.js
@@ -46,6 +46,102 @@
 *    ]
  */
 
-const combinate = (set, target) => {}
+function* times(set, target) {
+    for (i in set) {
+        for (let j = 1; j * set[i] <= target; j++) {
+            if (j * set[i] == target) {
+                yield {i: j, set: set[i]};
+            }
+        }
+    }
+
+    return true;
+}
+
+function decomp(set, n) {
+    let decomp = [];
+
+    let t = times(set, n);
+    console.log('############################## SET', set);
+    console.log('############################## N', n);
+
+    while (h = t.next()) {
+        console.log('############################## H', h);
+        let current = [];
+        if (!h.done) {
+            for (let j = 0; j < h.value.i; j++) {
+                current.push(h.value.set);
+            }
+            decomp.push(current);
+        } else {
+            break;
+        }
+    }
+
+    for (let i = n; i > 0; i--) {
+        let current = [];
+        // let j = i;
+        // console.log('############################## I', i);
+
+        // while (j > 0) {
+        //     current.push(j);
+        //     if (j == n) {
+        //         decomp.push(current);
+        //         current = [];
+        //     } else if(current.reduce((a, b) => a + b, 0) === n) {
+        //         decomp.push(current);
+        //         current = [];
+        //     } else if (current.reduce((a, b) => a + b, 0) > n) {
+        //         current = [];
+        //     } else {
+        //         continue;
+        //     }
+
+        //     j--;
+        // }
+    }
+
+    return decomp;
+}
+
+const combinate = (set, target) => {
+    let sets = decomp(set, target);
+
+    console.log('############################## SETS', sets);
+
+    // for (let i in set) {
+    //     let current = [];
+    //     let red = [];
+
+    //     for (let j = 1; j * set[i] <= target; j++) {
+    //         current.push(set[i]);
+    //         if (j * set[i] == target) {
+    //             sets.push(current);
+    //         } else if (set[i] < target) {
+    //             red.push(set[i]);
+    //         }
+    //     }
+    //     console.log('############################## RED', red);
+
+    //     let curr = red.reduce((a, b, i, arr) => {
+    //         if (a == target) {
+    //             arr.splice(arr.length - i - 1);
+    //         }
+    //         else {
+    //             return a + b
+    //         }
+    //     }, 0);
+    //     if (curr == target) {
+    //         sets.push(red);
+    //         red = [];
+    //     }
+    // }
+
+    // console.log('############################## DECOMP', sets);
+
+    // let sets = sumArray(set, target);
+
+    return sets;
+}
 
 module.exports = combinate
